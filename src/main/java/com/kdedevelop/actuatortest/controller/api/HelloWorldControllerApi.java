3a658641f998683533bcaa8199b55d6bbdd0f719
KDH <kde6571@gmail.com>
2023-10-15 11:04:03 +0900

    Feat: Add an endpoint for additional testing

--- src/main/java/com/kdedevelop/actuatortest/controller/api/HelloWorldControllerApi.java
+++ src/main/java/com/kdedevelop/actuatortest/controller/api/HelloWorldControllerApi.java
@@ -1,6 +1,8 @@
 package com.kdedevelop.actuatortest.controller.api;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloWorldControllerApi {
     @GetMapping("hello-world")
     public String helloWorld() {
         return "HI!";
+    }
+
+    @GetMapping("hello")
+    public String hello(@PathVariable String name) {
+        return "Welcome! [|=USER=|]!".replace("[|=USER=|]", name);
     }
 }
