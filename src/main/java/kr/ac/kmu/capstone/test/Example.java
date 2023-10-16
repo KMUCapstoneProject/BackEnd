8774ab632598468d1d4a5878236b5dff8dbf5a01
82103 <ghddnr6457@gmail.com>
2023-10-16 19:31:57 +0900

    커밋 테스트 겸 서버 DB에 Example Table 생성

--- src/main/java/kr/ac/kmu/capstone/test/Example.java
+++ src/main/java/kr/ac/kmu/capstone/test/Example.java
@@ -1,11 +1,14 @@
 package kr.ac.kmu.capstone.test;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Example {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
 }
