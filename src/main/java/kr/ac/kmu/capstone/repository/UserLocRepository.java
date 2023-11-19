71c6f5fb008a594e04b7ee3399f6a211b1900427
seon7129 <seon7129@naver.com>
2023-11-19 15:17:55 +0900

    delete userlocation

--- src/main/java/kr/ac/kmu/capstone/repository/UserLocRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kmu.Capstone.repository;
-
-import kr.ac.kmu.Capstone.entity.UserLocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLocRepository extends JpaRepository<UserLocation, Long> {
-}
