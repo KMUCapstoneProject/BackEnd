06413bcb15f2e60b64ac10f8ff234a84ae7246e8
wpghksdnd <oahfaj@naver.com>
2023-12-02 22:57:31 +0900

    upload excel, image file path 수정

--- src/main/java/kr/ac/kmu/capstone/image/FileUploadProperties.java
+++ src/main/java/kr/ac/kmu/capstone/image/FileUploadProperties.java
@@ -2,6 +2,7 @@ package kr.ac.kmu.Capstone.image;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 @ConfigurationProperties(prefix="file")
@@ -22,7 +23,6 @@ public class FileUploadProperties {
     public String getExcelUploadDir() {
         return excelUploadDir;
     }
-
     public void setExcelUploadDir(String excelUploadDir) {
         this.excelUploadDir = excelUploadDir;
     }
