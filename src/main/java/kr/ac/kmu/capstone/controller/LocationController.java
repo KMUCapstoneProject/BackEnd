ad0c1151e2d227807ecf4e78526f0d074bb6f736
seon7129 <seon7129@naver.com>
2024-05-30 21:40:29 +0900

    update api and msa

--- src/main/java/kr/ac/kmu/capstone/controller/LocationController.java
+++ src/main/java/kr/ac/kmu/capstone/controller/LocationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/location")
+@RequestMapping("/location-service/location")
 public class LocationController {
 
     @Autowired
