fa5b1ca775c667848afd6c596bb7cac034738bee
SkyLife* <112855505+hong6457@users.noreply.github.com>
2024-06-03 01:48:44 +0900

    update FirebaseConfig, path rebuilding

--- src/main/java/kr/ac/kmu/Capstone/config/FirebaseConfig.java
+++ src/main/java/kr/ac/kmu/Capstone/config/FirebaseConfig.java
@@ -5,16 +5,18 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/capstonetest-5d396-firebase-adminsdk-7g8ii-1be48f0250.json");
+        ClassPathResource resource = new ClassPathResource("capstonetest-5d396-firebase-adminsdk-7g8ii-1be48f0250.json");
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
